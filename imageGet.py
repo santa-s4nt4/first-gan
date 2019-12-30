8fc9180b71dc36b93414835d3b77522a088c3fd2
santa-sukitoku <santanaruse@gmail.com>
2019-12-30 21:31:01 +0900

    画像取得実装

--- imageGet.py
+++ imageGet.py
@@ -1,1 +1,7 @@
 from google_images_download import google_images_download
+
+response = google_images_download.googleimagesdownload()
+
+arguments = {"keywords": "金閣寺,Kinkaku,kinkakuji", "limit": 10000,
+             "chromedriver": "C:\\Users\\santa\\chromedriver_win32\\chromedriver.exe", "print_urls": True}
+response.download(arguments)
